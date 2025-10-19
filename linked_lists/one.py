b1094a814e0068220b888bdcb79d3894e267bb1c
Linden <lindenpowell218@gmail.com>
2025-10-19 18:49:14 +0800

    added display to see if it works just fine

--- linked_lists/one.py
+++ linked_lists/one.py
@@ -7,8 +7,32 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+    
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.tail = new_node
+            self.head = new_node
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.tail = new_node
+            self.head = new_node
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=">")
+            current = current.next
+        print("None")
    
 sushi_preparation = LinkedList()
 sushi_preparation.insert_at_end("prepare")
 sushi_preparation.insert_at_end("roll")
 sushi_preparation.insert_at_beginning("assemble")
+sushi_preparation.display()
