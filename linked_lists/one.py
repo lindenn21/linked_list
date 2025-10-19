f680fdb0de8f5297e76521f38b249b4830629708
Linden <lindenpowell218@gmail.com>
2025-10-19 18:42:39 +0800

    added the sample code provided

--- linked_lists/one.py
+++ linked_lists/one.py
@@ -8,3 +8,7 @@ class LinkedList:
         self.head = None
         self.tail = None
    
+sushi_preparation = LinkedList()
+sushi_preparation.insert_at_end("prepare")
+sushi_preparation.insert_at_end("roll")
+sushi_preparation.insert_at_beginning("assemble")
